2ea9cb09283035bd2dd3c34410988985963f74f0
PosazhennikovSA <posazhennikovsa@gmail.com>
2018-02-26 02:03:32 +0400

    Screensaver stub.

---
+++ Screensaver/main.cpp
@@ -0,0 +1,50 @@
+#include <windows.h>
+
+#include <scrnsave.h>
+#include <commctrl.h>
+
+#pragma comment(lib, "scrnsave.lib")
+#pragma comment(lib, "ComCtl32.lib")
+
+LONG WINAPI ScreenSaverProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+    case WM_CREATE:				// Creation of the screen saver window
+    {
+    }
+    break;
+    case WM_ERASEBKGND:			// Erases the screen saver background
+    {
+    }
+    break;
+    case WM_TIMER:				// Handles the timer
+    {
+    }
+    break;
+    case WM_DESTROY:			// Cleans up the screen saver window
+    {
+    }
+    break;
+    case WM_PAINT:				// Draws to the window
+    {
+    }
+    break;
+    default:
+    {
+        return DefScreenSaverProc(hWnd, message, wparam, lparam);
+    }
+    }
+
+    return 0;
+}
+
+BOOL WINAPI ScreenSaverConfigureDialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    return FALSE;
+}
+
+BOOL WINAPI RegisterDialogClasses(HANDLE hInst)
+{
+    return TRUE;
+}
